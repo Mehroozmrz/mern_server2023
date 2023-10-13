9f09ba62c7450813c62b966cde635889cf37028d
erpproject12 <139426885+erpproject12@users.noreply.github.com>
2023-10-13 11:45:29 +0530

    server-studentController

--- Controllers/StdudentController.js
+++ Controllers/StdudentController.js
@@ -94,28 +94,28 @@ const Get_Single_Student = async (req,res) => {
 
 
 
-// const Get_Single_Student_Field = async (req, res) => {
-//   try {
-//     const student = await Student.findById(req.params.id);
-//     if (!student) {
-//       res.status(404).send("Student Not Found");
-//     } else {
-//       const specificField1 = student.name; // Replace 'fieldName' with the actual field name
-//       const specificField2 = student.email; // Replace 'fieldName' with the actual field name
+const Get_Single_Student_Field = async (req, res) => {
+  try {
+    const student = await Student.findById(req.params.id);
+    if (!student) {
+      res.status(404).send("Student Not Found");
+    } else {
+      const specificField1 = student.name; // Replace 'fieldName' with the actual field name
+      const specificField2 = student.email; // Replace 'fieldName' with the actual field name
 
-//       const responseObj = {
-//         field1: specificField1,
-//         field2: specificField2,
-//       };
+      const responseObj = {
+        field1: specificField1,
+        field2: specificField2,
+      };
 
-//       // Send the specific field as the response
-//       res.json(responseObj);
-//     }
-//   } catch (error) {
-//     console.error(error.message);
-//     res.status(500).send("Internal some error occured");
-//   }
-// };
+      // Send the specific field as the response
+      res.json(responseObj);
+    }
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal some error occured");
+  }
+};
 
 module.exports = { StudentInsert, DisplayStudent,DeleteStudent,StudentUpdate,Get_Single_Student };
 
